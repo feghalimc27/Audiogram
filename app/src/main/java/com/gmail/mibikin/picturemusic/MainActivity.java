d7fb757d50fdbf49f9e7100f66936bdc4b668a25
Mario Feghali <mibikin@gmail.com>
2016-01-29 01:06:12 -0700

    Implemented partial design and navigation.

--- app/src/main/java/com/gmail/mibikin/picturemusic/MainActivity.java
+++ app/src/main/java/com/gmail/mibikin/picturemusic/MainActivity.java
@@ -1,5 +1,7 @@
 package com.gmail.mibikin.picturemusic;
 
+import android.content.Intent;
+import android.media.audiofx.BassBoost;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -16,13 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(R.drawable.mediumerlogotranssexual);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "This will let you select a picture.", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -34,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+/*
+    public void goToSettings(View view){
+        Intent intent = new Intent(this, SettingsActivity.class);
+    }
+*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -41,12 +49,23 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                this.startActivity(intent);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+/*
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+        */
+
+        return true;
     }
 }
