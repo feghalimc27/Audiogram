b35f25eed0ee5d596500da825aad893a5e343895
Mario <mario.feghali@ucdenver.edu>
2016-01-30 16:37:40 -0700

    Adjusted UI, Implemented camera, Potential saving code.

--- app/src/main/java/com/gmail/mibikin/picturemusic/MainActivity.java
+++ app/src/main/java/com/gmail/mibikin/picturemusic/MainActivity.java
@@ -2,17 +2,28 @@ package com.gmail.mibikin.picturemusic;
 
 import android.content.Intent;
 import android.media.audiofx.BassBoost;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_FROM_CAMERA = 112;
+    private Uri fileUri;
+    String image_path = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,10 +38,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "This will let you select a picture.", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(intent);
+                //Snackbar.make(view, "This will let you select a picture.", Snackbar.LENGTH_LONG)
+                //        .setAction("Action", null).show();
+                fromCamera();
             }
         });
     }
@@ -41,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
 /*
     public void goToSettings(View view){
         Intent intent = new Intent(this, SettingsActivity.class);
@@ -72,4 +83,32 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
+
+    public void fromCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+        getOutputMediaFile();
+    }
+
+    private static Uri getOutputMediaFileUri() {
+        return Uri.fromFile(getOutputMediaFile());
+    }
+
+    private static File getOutputMediaFile() {
+
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "audiogram");
+
+        if(! mediaStorageDir.exists()) {
+            if(! mediaStorageDir.mkdirs()){
+                Log.d("audiogram", "failed  to create directory!");
+                return null;
+            }
+        }
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        File mediaFile;
+        mediaFile = new File(mediaStorageDir.getPath() + File.separator + "AG_I_" + timeStamp + ".jpg");
+        return mediaFile;
+    }
+
 }
